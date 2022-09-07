32ddfc08104b16c9daa2bc5908bd74df0b0a4ce8
matheus <matheus.ecomp@gmail.com>
2022-09-06 23:25:38 -0300

    debug e criação da sidebar

--- src/pages/Charts/Bar.jsx
+++ src/pages/Charts/Bar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Cart() {
+function Bar() {
   return (
     <div>
       
@@ -8,4 +8,4 @@ function Cart() {
   )
 }
 
-export default Cart
+export default Bar
--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Cart() {
+const Ecommerce = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ function Cart() {
   )
 }
 
-export default Cart
+export default Ecommerce
